30aa4b0710e4c9bb9ad12d9cf5420a8c4493b66a
Marco <marcolinonft@gmail.com>
2024-10-15 17:12:21 +0200

    COMMIT 2 BACKEND

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export const PORT = 5555;
 
-export const mongoDBURL = "mongodb+srv://marco:CX6gUMCVDhW8qykT@santamarta.uxxxq.mongodb.net/<nome-database>?retryWrites=true&w=majority&tls=true&appName=Santamarta";
+export const mongoDBURL = 
+"mongodb+srv://marco:CX6gUMCVDhW8qykT@santamarta.uxxxq.mongodb.net/?retryWrites=true&w=majority&tls=true&tlsVersion=1.2";
