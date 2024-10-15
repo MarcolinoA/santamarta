387f83c789b9a1032e81feb91776aaa2c569bccd
Marco <marcolinonft@gmail.com>
2024-10-15 17:01:09 +0200

    commit for render bugs backend

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL =
-	"mongodb+srv://marco:CX6gUMCVDhW8qykT@santamarta.uxxxq.mongodb.net/?retryWrites=true&w=majority&appName=Santamarta";
+export const mongoDBURL = "mongodb+srv://marco:CX6gUMCVDhW8qykT@santamarta.uxxxq.mongodb.net/<nome-database>?retryWrites=true&w=majority&tls=true&appName=Santamarta";
