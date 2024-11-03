a5a278a1584bb4c25c7cf914f355ce64a85f2b1f
MarcolinoA <marcolinonft@gmail.com>
2024-11-03 13:42:31 +0100

    css styles

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -1,92 +1,94 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { useSearchParams } from "next/navigation"; 
+import { useSearchParams } from "next/navigation";
 import stylesHeader from "../../Styles/HomePage/Header.module.css";
 import { imageServices } from "../../services/apiImagesServices";
 import PriorityBtn from "../shared/btns/PriorityBtn";
 import HeaderBtn from "../shared/btns/HeaderBtn";
-
+import uva from "../../../public/festadelluva.jpeg"
 const FALLBACK_IMAGE =
-	"https://scuola-santamarta.s3.eu-north-1.amazonaws.com/OpenDay.jpeg";
+  "https://scuola-santamarta.s3.eu-north-1..com/OpenDay.jpeg";
 
 const HomePage = () => {
-	const [imageSrc, setImageSrc] = useState<string>(FALLBACK_IMAGE);
-	const [loading, setLoading] = useState<boolean>(true);
+  const [imageSrc, setImageSrc] = useState<string>(FALLBACK_IMAGE);
+  const [loading, setLoading] = useState<boolean>(true);
 
-	const options = [
-		{
-			label: "Registrati",
-			href: "/account/pages/signup",
-			dataid: "signup-btn",
-		},
-		{ label: "Accedi", href: "/account/pages/signin", dataid: "signin-btn" },
-		{ label: "Esci", href: "/account/pages/logout", dataid: "logout-btn" },
-		{
-			label: "Elimina",
-			href: "/account/pages/deleteAccount",
-			dataid: "delete-account-btn",
-		},
-	];
+  const options = [
+    {
+      label: "Registrati",
+      href: "/account/pages/signup",
+      dataid: "signup-btn",
+    },
+    { label: "Accedi", href: "/account/pages/signin", dataid: "signin-btn" },
+    { label: "Esci", href: "/account/pages/logout", dataid: "logout-btn" },
+    {
+      label: "Elimina",
+      href: "/account/pages/deleteAccount",
+      dataid: "delete-account-btn",
+    },
+  ];
 
-	const option = [
-		{
-			href: "/home/homeImgList",
-			style: {
-				position: "absolute" as const,
-			},
-		},
-	];
+  const option = [
+    {
+      href: "/home/homeImgList",
+      style: {
+        position: "absolute" as const,
+      },
+    },
+  ];
 
-	useEffect(() => {
-		const fetchActiveImage = async () => {
-			setLoading(true);
-			try {
-				const image = await imageServices.getActiveImage();
-				// Se l'immagine non è disponibile, imposta l'immagine di fallback
-				setImageSrc(image.image ? image.image : FALLBACK_IMAGE);
-			} catch (error) {
-				// In caso di errore, imposta l'immagine di fallback
-				console.error("Error fetching active image:", error);
-				setImageSrc(FALLBACK_IMAGE);
-			} finally {
-				setLoading(false);
-			}
-		};
+  useEffect(() => {
+    const fetchActiveImage = async () => {
+      setLoading(true);
+      try {
+        const image = await imageServices.getActiveImage();
+        // Se l'immagine non è disponibile, imposta l'immagine di fallback
+        setImageSrc(image.image ? image.image : FALLBACK_IMAGE);
+      } catch (error) {
+        // In caso di errore, imposta l'immagine di fallback
+        console.error("Error fetching active image:", error);
+        setImageSrc(FALLBACK_IMAGE);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-		fetchActiveImage();
-	}, []);
+    fetchActiveImage();
+  }, []);
 
-	const searchParams = useSearchParams();
-	let username = "";
+  const searchParams = useSearchParams();
+  let username = "";
 
-	if (searchParams) {
-		username = searchParams.get("username") || "";
-	}
+  if (searchParams) {
+    username = searchParams.get("username") || "";
+  }
 
-	return (
-		<div className={stylesHeader.headerContainer}>
-			{loading ? (
-				<div data-id="loading-spinner" className={stylesHeader.loader}></div>
-			) : (
-				<Image
-					src="https://scuola-santamarta.s3.eu-north-1.amazonaws.com/OpenDay.jpeg"
-					alt="test"
-					layout="fill"
-					objectFit="cover"
-					quality={100}
-					priority={true}
-					data-id="home-page-img"
-				/>	
-			)}
-			<HeaderBtn
-				isLoggedIn={true}
-				username={(username as string) || ""}
-				options={options}
-			/>
-			<PriorityBtn option={option} />
-		</div>
-	);
+  return (
+    <div className={stylesHeader.headerContainer}>
+      {loading ? (
+        <div data-id="loading-spinner" className={stylesHeader.loader}></div>
+      ) : (
+        <Image
+          src={imageSrc}
+          alt="test"
+          layout="responsive"
+          width={800} 
+          height={600}
+          quality={100}
+          priority={true}
+          data-id="home-page-img"
+		  className={stylesHeader.customImage}
+        />
+      )}
+      <HeaderBtn
+        isLoggedIn={true}
+        username={(username as string) || ""}
+        options={options}
+      />
+      <PriorityBtn option={option} />
+    </div>
+  );
 };
 
 export default HomePage;
