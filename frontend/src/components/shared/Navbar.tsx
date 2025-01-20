3a828a7f7dcab95cbbc0e3f09beefe887ae1c7d4
MarcolinoA <marcolinonft@gmail.com>
2025-01-20 23:21:00 +0100

    navbar

--- frontend/src/components/shared/Navbar.tsx
+++ frontend/src/components/shared/Navbar.tsx
@@ -1,87 +1,85 @@
 "use client";
-
 import React, { useState, useEffect, useRef } from "react";
-import { useRouter } from "next/navigation"; // Usa il router specifico per il client
-import { usePathname } from "next/navigation"; // Per ottenere la rotta corrente
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import stylesNavbar from "../../Styles/Navbar.module.css";
+import stylesBtn from "../../Styles/HomePage/Btns/HeaderBtn.module.css";
 import { FiMenu } from "react-icons/fi";
+import Link from "next/link";
+import { useAuthentication } from "../../hooks/useAuthentications";
 
 interface MenuItem {
   name: string;
   route: string;
 }
 
-const MobileMenu: React.FC<{
-  menuItems: MenuItem[];
-  activeIndex: number;
-  setActiveIndex: (index: number) => void;
-  isOpen: boolean;
-  toggleMenu: () => void;
-}> = ({ menuItems, activeIndex, setActiveIndex, isOpen, toggleMenu }) => {
-  const router = useRouter();
-
-  return (
-    <div
-      className={`${stylesNavbar.mobileMenu} ${
-        isOpen ? stylesNavbar.open : stylesNavbar.close
-      }`}
-    >
-      {menuItems.map((item, index) => (
-        <div
-          key={index}
-          className={`${stylesNavbar.navbarItem} ${
-            activeIndex === index ? stylesNavbar.active : ""
-          }`}
-          onClick={() => {
-            setActiveIndex(index);
-            toggleMenu();
-            router.push(item.route); // Usa router.push solo lato client
-          }}
-        >
-          <span className={stylesNavbar.navbarText}>{item.name}</span>
-        </div>
-      ))}
-    </div>
-  );
-};
-
 const Navbar: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState<number>(0);
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [isProfileDropdownVisible, setIsProfileDropdownVisible] =
+    useState<boolean>(false);
+  const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
+  const [isDropdownClosing, setIsDropdownClosing] = useState(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
   const menuRef = useRef<HTMLDivElement>(null);
-  const rawPathname = usePathname(); // Ottieni la rotta corrente
-  const pathname = rawPathname ?? "/"; // Usa "/" come valore di fallback se pathname è null
+  const rawPathname = usePathname();
+  const pathname = rawPathname ?? "/";
   const router = useRouter();
+  const { isAuthenticated, username } = useAuthentication();
 
   const menuItems: MenuItem[] = [
     { name: "Home", route: "/" },
     { name: "La scuola", route: "/lascuola" },
     { name: "Documents", route: "/documents" },
-    { name: "Components", route: "/components" },
-    { name: "Calendar", route: "/calendar" },
-    { name: "Charts", route: "/charts" },
+    { name: "Profilo", route: "#" },
   ];
 
-  // Funzione per trovare l'indice corrispondente alla rotta corrente
-  const findActiveIndex = (path: string): number => {
-    const index = menuItems.findIndex((item) => item.route === path);
-    return index >= 0 ? index : 0; // Torna a 0 (Home) se la rotta non è trovata
-  };
-
-  useEffect(() => {
-    // Aggiorna l'indice attivo in base alla rotta corrente
-    setActiveIndex(findActiveIndex(pathname));
-  }, [pathname]); // Aggiorna quando cambia pathname
+  const dropdownOptions = [
+    { label: "Impostazioni", href: "/settings", dataid: "settings" },
+    { label: "Logout", href: "/logout", dataid: "logout" },
+  ];
 
   const toggleMenu = () => {
     setIsMenuOpen((prev) => !prev);
   };
 
+  const toggleProfileDropdown = () => {
+    if (isMobile) {
+      // Gestione del dropdown per la versione mobile
+      if (isProfileDropdownVisible) {
+        setIsDropdownClosing(true); // Inizia l'animazione di chiusura
+        setTimeout(() => {
+          setIsProfileDropdownVisible(false); // Nascondi il menu dopo l'animazione
+          setIsDropdownClosing(false); // Resetta lo stato di chiusura
+        }, 300); // Durata dell'animazione (300ms)
+      } else {
+        setIsProfileDropdownVisible(true); // Mostra il menu
+      }
+    } else {
+      // Gestione del dropdown per la versione desktop
+      setIsDropdownVisible((prev) => !prev);
+    }
+  };
+
   const handleClickOutside = (event: MouseEvent) => {
     if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
       setIsMenuOpen(false);
+      setIsProfileDropdownVisible(false);
     }
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Definiamo 768px come punto di interruzione per la versione mobile
+    };
+
+    // Chiama handleResize all'inizio per impostare lo stato corretto
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
@@ -91,19 +89,64 @@ const Navbar: React.FC = () => {
     };
   }, []);
 
+  const findActiveIndex = (path: string): number => {
+    const index = menuItems.findIndex((item) => item.route === path);
+    return index >= 0 ? index : 0;
+  };
+
+  useEffect(() => {
+    setActiveIndex(findActiveIndex(pathname));
+  }, [pathname]);
+
   return (
     <div className={stylesNavbar.navbar} ref={menuRef}>
       <div className={stylesNavbar.hamburger} onClick={toggleMenu}>
         <FiMenu size={30} color="#000" />
       </div>
 
-      <MobileMenu
-        menuItems={menuItems}
-        activeIndex={activeIndex}
-        setActiveIndex={setActiveIndex}
-        isOpen={isMenuOpen}
-        toggleMenu={toggleMenu}
-      />
+      <div
+        className={`${stylesNavbar.mobileMenu} ${
+          isMenuOpen ? stylesNavbar.open : stylesNavbar.close
+        }`}
+      >
+        {menuItems.map((item, index) => (
+          <div
+            key={index}
+            className={`${stylesNavbar.navbarItem} ${
+              activeIndex === index ? stylesNavbar.active : ""
+            }`}
+            onClick={() => {
+              setActiveIndex(index);
+              if (item.name === "Profilo") {
+                toggleProfileDropdown();
+              } else {
+                toggleMenu();
+                setIsProfileDropdownVisible(false);
+                router.push(item.route);
+              }
+            }}
+          >
+            <span className={stylesNavbar.navbarText}>{item.name}</span>
+          </div>
+        ))}
+
+        {/* Mostra gli elementi della tendina sotto "Profilo" */}
+        <div
+          className={`${stylesNavbar.dropdownSubMenu} ${
+            isProfileDropdownVisible ? stylesNavbar.open : ""
+          }`}
+        >
+          {dropdownOptions.map((option, index) => (
+            <div key={index} className={stylesNavbar.navbarItem}>
+              <button className={stylesBtn.dropdownButton}>
+                <Link className={stylesBtn.dropdownLinks} href={option.href}>
+                  {option.label}
+                </Link>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
 
       <div className={stylesNavbar.navbarMenu}>
         {menuItems.map((item, index) => (
@@ -114,7 +157,9 @@ const Navbar: React.FC = () => {
             }`}
             onClick={() => {
               setActiveIndex(index);
-              router.push(item.route);
+              if (item.name === "Profilo")
+                toggleProfileDropdown(); // Gestisce sia mobile che desktop
+              else router.push(item.route);
             }}
           >
             <div className={stylesNavbar.spanContainer}>
@@ -122,10 +167,21 @@ const Navbar: React.FC = () => {
             </div>
           </div>
         ))}
-        <div
-          className={stylesNavbar.navbarActiveIndicator}
-          style={{ transform: `translateX(${activeIndex * 100}%)` }}
-        />
+
+        {/* Dropdown desktop per "Profilo" */}
+        {isDropdownVisible && (
+          <div className={stylesBtn.dropdownMenu}>
+            {dropdownOptions.map((option, index) => (
+              <div key={index} className={stylesBtn.drop}>
+                <button className={stylesBtn.dropdownButton}>
+                  <Link className={stylesBtn.dropdownLinks} href={option.href}>
+                    {option.label}
+                  </Link>
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
